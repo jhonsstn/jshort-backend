210af23245b55cfc52ac5d5a1bc2429cff77a405
Jhon Santana <jhonsstn@gmail.com>
2022-06-13 19:11:32 -0300

    update port heroku

--- src/server.js
+++ src/server.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server started on port 3000');
 });
