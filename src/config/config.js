ac96c777dc7f6c9b83ab873704f5c75a549e1c1e
Jhon Santana <jhonsstn@gmail.com>
2022-06-17 15:06:22 -0300

    chore: add mocha, chai and sinon

--- src/config/config.js
+++ src/config/config.js
@@ -1,18 +1,13 @@
-require('dotenv').config({ path: __dirname + '/../../.env' });
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
 
 module.exports = {
   development: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
+    username: process.env.DEV_USERNAME,
+    password: process.env.DEV_PASSWORD,
+    database: process.env.DEV_DATABASE,
+    host: process.env.DEV_HOST,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   },
   test: {
     username: process.env.USERNAME,
